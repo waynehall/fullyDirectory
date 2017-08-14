36e4eeaba2239a7bff9ac0fe7aac8cc848316a62
waynehall <waynehall@Waynes-MacBook-Pro.local>
2017-08-14 11:03:22 -0700

    first commit

---
+++ directory.js
@@ -0,0 +1,126 @@
+// Client ID and API key from the Developer Console
+var userlist = [{name: "placeholder"}]
+      var CLIENT_ID = '870607351135-6gpqhneibup84op6n2o5dq19gutauaqo.apps.googleusercontent.com';
+
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/admin/directory_v1/rest"];
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      var SCOPES = 'https://www.googleapis.com/auth/admin.directory.user.readonly';
+
+      var authorizeButton = document.getElementById('authorize-button');
+      var signoutButton = document.getElementById('signout-button');
+
+      /**
+       *  On load, called to load the auth2 library and API client library.
+       */
+      function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+      }
+
+      /**
+       *  Initializes the API client library and sets up sign-in state
+       *  listeners.
+       */
+      function initClient() {
+        gapi.client.init({
+          discoveryDocs: DISCOVERY_DOCS,
+          clientId: CLIENT_ID,
+          scope: SCOPES
+        }).then(function () {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+          // Handle the initial sign-in state.
+          updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+          authorizeButton.onclick = handleAuthClick;
+          signoutButton.onclick = handleSignoutClick;
+        });
+      }
+
+      /**
+       *  Called when the signed in status changes, to update the UI
+       *  appropriately. After a sign-in, the API is called.
+       */
+      function updateSigninStatus(isSignedIn) {
+        if (isSignedIn) {
+          authorizeButton.style.display = 'none';
+          signoutButton.style.display = 'block';
+          listUsers();
+        } else {
+          authorizeButton.style.display = 'block';
+          signoutButton.style.display = 'none';
+        }
+      }
+
+      /**
+       *  Sign in the user upon button click.
+       */
+      function handleAuthClick(event) {
+        gapi.auth2.getAuthInstance().signIn();
+      }
+
+      /**
+       *  Sign out the user upon button click.
+       */
+      function handleSignoutClick(event) {
+        gapi.auth2.getAuthInstance().signOut();
+      }
+
+      /**
+       * Append a pre element to the body containing the given message
+       * as its text node. Used to display the results of the API call.
+       *
+       * @param {string} message Text to be placed in pre element.
+       */
+      function appendPre(message) {
+        var pre = document.getElementById('content');
+        var textContent = document.createTextNode(message + '\n');
+        pre.appendChild(textContent);
+      }
+
+      /**
+       * Print the first 10 users in the domain.
+       */
+      function listUsers() {
+        gapi.client.directory.users.list({
+          'customer': 'my_customer',
+          'maxResults': 100,
+          'orderBy': 'email'
+        }).then(function(response) {
+          var users = response.result.users;
+          appendPre('Users:');
+          if (users && users.length > 0) {
+            for (i = 0; i < users.length; i++) {
+                //console.log(user);
+              var user = users[i];
+              userlist.push(user)
+              appendPre('-' + user.primaryEmail + ' (' + user.name.fullName + ')');
+              if (user.organizations){
+                  appendPre(user.organizations[0].title);
+              };
+              if (user.thumbnailPhotoUrl){
+                  appendPre(user.thumbnailPhotoUrl)
+              }
+            }
+          } else {
+            appendPre('No users found.');
+          }
+        });
+      }
+
+var app = angular.module('MyApp', []);
+app.controller('AppController', function ($scope){      
+    $scope.msg = "don't give up!"
+    $scope.userlist = userlist
+    $scope.logger = function(){
+        console.log(userlist)
+    }
+
+    $scope.$watch('userlist', function(){
+        console.log($scope.userlist);
+    })
+    
+    
+})
